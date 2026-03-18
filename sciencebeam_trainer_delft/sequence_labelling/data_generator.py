d39f6a61699eb2bb0de342e9fce02aebad99fc16
Daniel Ecer <de-code@users.noreply.github.com>
2026-03-18 09:25:17 +0000

    linting: data generator

--- sciencebeam_trainer_delft/sequence_labelling/data_generator.py
+++ sciencebeam_trainer_delft/sequence_labelling/data_generator.py
@@ -584,8 +584,6 @@ class DataGenerator(keras.utils.Sequence):
             max_length_x += 1
             extend = True
 
-        batch_y = None
-
         sub_f = None
         if (
                 self.preprocessor.return_features
@@ -624,30 +622,32 @@ class DataGenerator(keras.utils.Sequence):
         if self.preprocessor.return_casing:
             batch_a = to_batch_casing(x_tokenized, max_length_x)
 
-        batch_y = None
+        batch_y_ndarray: Optional[np.ndarray] = None
         # store tag embeddings
         if self.y is not None:
-            batch_y = take_with_offset(self.y, window_indices_and_offsets)
-            max_length_y = max((len(y_row) for y_row in batch_y))
+            batch_y_list = take_with_offset(self.y, window_indices_and_offsets)
+            max_length_y = max((len(y_row) for y_row in batch_y_list))
             if self.max_sequence_length and max_length_y > self.max_sequence_length:
                 max_length_y = self.max_sequence_length
                 # truncation of sequence at max_sequence_length
-                batch_y = truncate_batch_values(batch_y, self.max_sequence_length)
+                batch_y_list = truncate_batch_values(batch_y_list, self.max_sequence_length)
 
-            batches, batch_y = self.preprocessor.transform(
+            batches, batch_y_list = self.preprocessor.transform(
                 padded_batch_text_list,
-                batch_y,
+                batch_y_list,
                 extend=extend,
                 label_indices=not self.use_chain_crf
             )
-            truncated_batch_y = truncate_batch_values(batch_y, max_length_x)
+            truncated_batch_y = truncate_batch_values(batch_y_list, max_length_x)
             try:
-                batch_y = np.asarray(truncated_batch_y, dtype=np.int32)
+                batch_y_ndarray = np.asarray(truncated_batch_y, dtype=np.int32)
             except ValueError:
                 max_length_y = max(len(y_row) for y_row in truncated_batch_y)
-                batch_y = np.zeros((len(truncated_batch_y), max_length_y), dtype=np.int32)
+                batch_y_ndarray = np.zeros(
+                    (len(truncated_batch_y), max_length_y), dtype=np.int32
+                )
                 for i, y_row in enumerate(truncated_batch_y):
-                    batch_y[i, :len(y_row)] = y_row
+                    batch_y_ndarray[i, :len(y_row)] = y_row
         else:
             batches = self.preprocessor.transform(
                 padded_batch_text_list, extend=extend
@@ -686,7 +686,7 @@ class DataGenerator(keras.utils.Sequence):
                 batch_features.shape,
                 batch_features.dtype
             )
-            if batch_features.dtype == np.object0:
+            if batch_features.dtype == np.object_:
                 LOGGER.warning(
                     'invalid object type of batch_features sample data[0]: %s',
                     batch_features[0][0:min(2, len(batch_features[0]))]
@@ -702,11 +702,11 @@ class DataGenerator(keras.utils.Sequence):
             LOGGER.debug('inputs shapes: %s', [x.shape for x in input_nparrays])
             LOGGER.debug('inputs dtype: %s', [x.dtype for x in input_nparrays])
             for index, input_array in enumerate(input_nparrays):
-                if input_array.dtype == np.object0:
+                if input_array.dtype == np.object_:
                     LOGGER.warning(
                         'invalid object type inputs[%d] sample data[0]: %s',
                         index,
                         input_array[0][0:min(2, len(input_array[0]))]
                     )
 
-        return inputs, batch_y
+        return inputs, batch_y_ndarray
